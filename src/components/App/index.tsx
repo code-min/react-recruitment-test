445a94e8850b418b769a796e210e8761e4c03732
code-min <50967209+code-min@users.noreply.github.com>
2019-09-23 15:07:04 +0200

    updated home page

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,26 +1,42 @@
 import './App.scss';
 
 import React from 'react';
+import Card from '../Cards/Card';
+import Cars from '../../cars.json';
 
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/logo-2.svg';
+import menu from '../../assets/list-menu.svg';
+import { Link } from 'react-router-dom';
+
+//function to get the car manufacturing year 
+function getYear(year: any) {
+  return new Date(year).getFullYear();
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          For further information open <code>README.md</code> please.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-light">
+        <img src={logo} width="125px" alt="logo"></img>
+        <span className="nav-center">Autosuche</span>
+        <Link to={"/contact"} >
+        <img src={menu} width="15px" alt=""></img>
+        </Link>
+      </nav>
+
+      <main>
+        <div>
+          <h2>Wähle dein Traumauto</h2>
+        </div>
+
+        {/*Display card component multiple times, for all cars that are rentable*/}
+        <div className="card-group">
+        {Cars.map(Car => (Car.Rentable) ? 
+          (<Card name={Car.Name} jahr={getYear(Car.Year)}></Card>) : ""
+        )}
+        </div>
+      </main>
+
     </div>
   );
 }
