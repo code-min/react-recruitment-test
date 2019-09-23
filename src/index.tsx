78c293c5fab75de289aa50f21fa74a8af5bb6931
code-min <50967209+code-min@users.noreply.github.com>
2019-09-23 15:02:54 +0200

    routing

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import './index.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import Contact from './components/App/Contact/Contact';
 
 // Disable AXE for production
 if (process.env.NODE_ENV !== "production") {
@@ -12,7 +14,16 @@ if (process.env.NODE_ENV !== "production") {
   axe(React, ReactDOM, 1000);
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const routing = (
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
